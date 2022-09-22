e9a94b0d72965053ba9926db7df2deb7182800fb
TomasCarranzaC <tomascarranzac@gmail.com>
2022-09-22 20:42:10 +0000

    TESTS OK

--- src/app.py
+++ src/app.py
@@ -6,39 +6,43 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
+    all_members = jackson_family.get_all_members()
+    return jsonify(all_members), 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member', methods = ['POST'])
+def add_member():
+    new_member = request.get_json(force=True)
+    jackson_family.add_member(new_member)
+    return jsonify({}), 200
 
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    ex_member = jackson_family.delete_member(id)
+    return jsonify({"done":True}), 200
 
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id) :
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+    
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
